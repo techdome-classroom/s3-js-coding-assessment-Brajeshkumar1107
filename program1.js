f21b0702329ea1a30c0f7f26961e75fa1d59e5d4
Brajesh Kumar <113904519+Brajeshkumar1107@users.noreply.github.com>
2024-09-30 15:58:14 +0530

    Update program1.js

--- program1.js
+++ program1.js
@@ -3,7 +3,25 @@
  * @return {boolean}
  */
 var isValid = function(s) {
+    let stack = [];
+    let bracketMap = {
+        ')': '(',
+        '}': '{',
+        ']': '['
+    };
     
+    for (let char of s) {
+        if (char in bracketMap) {
+            let topElement = stack.length > 0 ? stack.pop() : '#';
+            if (topElement !== bracketMap[char]) {
+                return false;
+            }
+        } else {
+            stack.push(char);
+        }
+    }
+    
+    return stack.length === 0;
 };
 
 module.exports = { isValid };
